f6f079e6ac3d928849d336a1f1b9b7eb1485afaf
bhaktatejas922 <bhaktatejas922@gmail.com>
2024-10-03 16:51:24 -0700

    hack to make import work in parents

--- findmy/__init__.py
+++ findmy/__init__.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory to sys.path
+parent_dir = Path(__file__).parent.parent
+if str(parent_dir) not in sys.path:
+    sys.path.insert(0, str(parent_dir))
+
 """A package providing everything you need to work with Apple's FindMy network."""
 
 from . import errors, keys, reports, scanner
--- findmy/reports/__init__.py
+++ findmy/reports/__init__.py
@@ -4,11 +4,12 @@ from .account import AppleAccount, AsyncAppleAccount
 from .anisette import BaseAnisetteProvider, RemoteAnisetteProvider
 from .state import LoginState
 from .twofactor import SmsSecondFactorMethod, TrustedDeviceSecondFactorMethod
-
+from .reports import LocationReport
 __all__ = (
     "AppleAccount",
     "AsyncAppleAccount",
     "LoginState",
+    "LocationReport",
     "BaseAnisetteProvider",
     "RemoteAnisetteProvider",
     "SmsSecondFactorMethod",
